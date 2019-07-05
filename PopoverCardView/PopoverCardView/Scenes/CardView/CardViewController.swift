441180546a363e48a8e66c1f3d39ccd0630fc3c4
Nguyen Duc Huy <ndhuy96@gmail.com>
2019-07-05 22:05:05 +0700

    Create CardView

---
+++ PopoverCardView/PopoverCardView/Scenes/CardView/CardViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CardViewController.swift
+//  PopoverCardView
+//
+//  Created by mac on 7/6/19.
+//  Copyright © 2019 sun. All rights reserved.
+//
+
+import UIKit
+
+final class CardViewController: UIViewController {
+    @IBOutlet weak var handleArea: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
